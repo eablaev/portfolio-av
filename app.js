1a842b65caeeb5bbfdb9f0bb7080b05ba0d15a49
shmerv <52586290+eablaev@users.noreply.github.com>
2026-04-26 18:14:57 -0500

    Update navigation history handling for buttons

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const aboutBtnContainer = document.getElementById('aboutBtnContainer');
 const projectsBtnContainer = document.getElementById('projectsBtnContainer');
-
 const messageEl = document.getElementById('welcomeMessageContainer');
 const aboutContent = document.getElementById('aboutContent');
 const projectsContainer = document.getElementById('projectsContainer')
-
 const closeBtnAbout = document.getElementById('closeBtnAbout');
 const closeBtnProjects = document.getElementById('closeBtnProjects');
-
 const lines = document.querySelectorAll('.line'); 
 
-
 aboutBtnContainer.addEventListener('click', () => {
-    history.pushState(null, '', '/portfolio-av/');
+    history.pushState(null, '', '#about');
     aboutBtnContainer.classList.add('slideOut')
     projectsBtnContainer.classList.add('slideOut');
     messageEl.classList.add('slideOut')
-   
     aboutBtnContainer.addEventListener('animationend', () => {
         messageEl.classList.remove('show');
         aboutContent.classList.add('show');
@@ -28,7 +23,6 @@ projectsBtnContainer.addEventListener('click', () => {
     aboutBtnContainer.classList.add('slideOut')
     projectsBtnContainer.classList.add('slideOut');
     messageEl.classList.add('slideOut')
-   
     projectsBtnContainer.addEventListener('animationend', () => {
         messageEl.classList.remove('show');
         projectsContainer.classList.add('show');
@@ -38,50 +32,38 @@ projectsBtnContainer.addEventListener('click', () => {
 closeBtnAbout.addEventListener('click', () => {  
     history.pushState(null, '', '/portfolio-av/');
     aboutContent.classList.add('slideOut');
-    
     aboutContent.addEventListener('animationend', () => {
         aboutContent.classList.remove('show');
         aboutContent.classList.remove('slideOut');
-        
         aboutBtnContainer.classList.add('slideIn');
         projectsBtnContainer.classList.add('slideIn');
-
         messageEl.classList.remove('slideOut');
         messageEl.classList.add('show');
-
         aboutBtnContainer.classList.remove('slideOut');
         projectsBtnContainer.classList.remove('slideOut');
-        
         aboutBtnContainer.addEventListener('animationend', () => {
             projectsBtnContainer.classList.remove('slideIn');
             aboutBtnContainer.classList.remove('slideIn');
         }, {once:true});
-
     }, { once: true });
 });
 
 closeBtnProjects.addEventListener('click', () => {  
     history.pushState(null, '', '/portfolio-av/');
     projectsContainer.classList.add('slideOut');
-    
     projectsContainer.addEventListener('animationend', () => {
         projectsContainer.classList.remove('show');
         projectsContainer.classList.remove('slideOut');
-        
         aboutBtnContainer.classList.add('slideIn');
         projectsBtnContainer.classList.add('slideIn');
-
         messageEl.classList.remove('slideOut');
         messageEl.classList.add('show');
-
         aboutBtnContainer.classList.remove('slideOut');
         projectsBtnContainer.classList.remove('slideOut');
-        
         aboutBtnContainer.addEventListener('animationend', () => {
             projectsBtnContainer.classList.remove('slideIn');
             aboutBtnContainer.classList.remove('slideIn');
         }, {once:true});
-
     }, { once: true });
 });
 
